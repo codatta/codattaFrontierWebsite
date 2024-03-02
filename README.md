99e4c3b7f0ac1207b57571fcd7bd1c55864c86d9
yangxiaohu <yangxiaohu2015@163.com>
2024-03-02 10:51:31 +0800

    initial

---
+++ README.md
@@ -0,0 +1,46 @@
+## Omnitags Platform Website
+
+This is a highly professional web front-end scaffolding solution that integrates advanced technologies, including:
+
+- [TypeScript](https://www.typescriptlang.org/): TypeScript provides type safety, reducing potential errors through static type checking. This contributes to improved code quality and maintainability.
+
+- [React](https://react.dev/): React is a top-tier JavaScript library known for its component-based development model and outstanding maintainability. It offers the ability to build modular, highly interactive, and dynamic web applications.
+
+- [Vite](https://vitejs.dev/): 快速构建工具
+
+- [Unocss](https://unocss.dev/): 原子样式方案，集成了tailwindcss，文档参考[tailwindcss](https://www.tailwindcss.cn/)即可
+
+- [Ant Design 5.0](https://ant-design.antgroup.com/index-cn): React ui组件库
+
+- [Meteors](https://ui.aceternity.com/components/meteors): 动效参考
+
+- [Valtio](https://valtio.pmnd.rs/): 简单好用的状态管理库
+
+- [lodash](https://www.lodashjs.com/): 一个一致性、模块化、高性能的 JavaScript 实用工具库
+
+- [postcss-px-viewport](https://evrone.com/blog/postcss-px-viewport): 页面适配方案
+
+## 视觉
+# 标准设计稿尺寸：1440 x 750
+# tailwindcss实现了大多数常用的css基础样式，比如flex
+
+## Usage
+
+```bash
+# 1. Install
+yarn
+
+# 2. Development
+yarn dev
+
+# 3. Production
+yarn build
+yarn preview
+
+# 4. Lint
+yarn lint --check
+yarn lint:fix --fix
+
+# 5. Testing with Eruda
+baseUrl + '?debug=1'
+```
---
+++ vite.config.ts
@@ -0,0 +1,76 @@
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import { presetUno, presetAttributify } from 'unocss'
+import Unocss from 'unocss/vite'
+import postcssImport from 'postcss-import'
+import postcssAutoPrefixer from 'autoprefixer'
+import postcssPxToViewport from 'postcss-px-to-viewport'
+
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    splitVendorChunkPlugin(),
+    react(),
+    // basicSsl(),
+    Unocss({
+      presets: [presetUno(), presetAttributify()],
+    }),
+  ],
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    // https: true,
+    open: false,
+  },
+  build: {
+    emptyOutDir: true,
+    outDir: 'dist',
+    assetsDir: 'assets',
+    assetsInlineLimit: 500000, // 26kb以下
+    cssCodeSplit: false, // css不拆分
+    terserOptions: {
+      compress: {
+        drop_console: true, // 从压缩后的代码中删除console函数调用
+        drop_debugger: true, // 从压缩后的代码中删除debugger关键词
+      },
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        //解决css文件压缩合并时的警告——warning: "@charset" must be the first rule in the file
+        {
+          postcssPlugin: 'internal:charset-removal',
+          AtRule: {
+            charset: (atRule) => {
+              if (atRule.name === 'charset') {
+                atRule.remove()
+              }
+            },
+          },
+        },
+        postcssImport,
+        postcssAutoPrefixer,
+        postcssPxToViewport({
+          viewportWidth: 1440,
+          viewportHeight: 750,
+          unitPrecision: 5,
+          viewportUnit: 'vw',
+          selectorBlackList: ['.ignore', '.hairlines'],
+          minPixelValue: 1,
+          mediaQuery: false,
+          exclude: /(node_module)/,
+        }),
+      ],
+    },
+  },
+})
