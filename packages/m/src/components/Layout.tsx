475b66f5b3e63fa5471b5e46ac58c5c03747a27d
markof <markof@163.com>
2024-06-13 14:30:39 +0800

    add channel cookie

--- packages/m/src/components/Layout.tsx
+++ packages/m/src/components/Layout.tsx
@@ -1,6 +1,9 @@
+import { getChannelCode } from '@/utils/channel'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
+  // 临时加在这里
+  getChannelCode()
   return (
     <div className="min-h-screen m-auto tracking-tight">
       <Outlet />
---
+++ packages/pc/src/utils/channel.ts
@@ -0,0 +1,18 @@
+'use client'
+
+import cookies from 'js-cookie'
+const CHANNEL_KEY = '_ch'
+
+export function getChannelCode() {
+  const querystirng = new URLSearchParams(location?.search)
+
+  // 这里处理了渠道相关的逻辑。如果url中有渠道信息，则渠道信息直接写到cookie中，否则从cookie中读取。
+  const cookieChannelCode = cookies.get(CHANNEL_KEY)
+  const queryChannelCode = querystirng.get(CHANNEL_KEY) || ''
+
+  if (queryChannelCode) {
+    cookies.set(CHANNEL_KEY, queryChannelCode, { expires: 365 })
+  }
+
+  return queryChannelCode || cookieChannelCode
+}
