11ea0196d51ccac9229f767ae65a719188e6ea83
yangxiaohu <yangxiaohu2015@163.com>
2024-12-09 15:50:54 +0800

    feat: add section 3 layout and components

---
+++ src/components/section-3/index.tsx
@@ -0,0 +1,61 @@
+import { cn } from '@udecode/cn'
+
+import DynamicSvg from '@/components/dynamic-svg'
+
+export default function Section({ className }: { className?: string }) {
+  return (
+    <div className={cn('text-center', className)}>
+      <p className="font-medium text-xl tracking-wide">Vertical AI Agents</p>
+      <h3 className="font-extrabold text-[32px] leading-10 mt-4">
+        The New Saas Build Vertical AI Agents
+      </h3>
+      <p className="text-base tracking-wide mt-4">
+        Vertical AI Agents are specialized AI entities tailored for specific
+        industries. For instance, in healthcare, they assist with medical
+        diagnoses using patient data,
+      </p>
+      <div className="text-left mt-[48px] flex flex-col gap-6">
+        <Card
+          iconName="rect-ai"
+          title="Industry-Specific Intelligence"
+          des="Create Al agents with deep vertical knowledge that understand specific industry contexts, workflows, and challenges."
+        />
+        <Card
+          iconName="rect-ball"
+          title="Domain Expert Integration"
+          des="Seamlessly combine Al capabilities with human domain expertise to build solutions that truly understand your industry."
+        />
+        <Card
+          iconName="rect-cross"
+          title="Automated Workflows"
+          des="Transform traditional SaaS functions into intelligent agents that automate complex industry-specific processes."
+        />
+      </div>
+    </div>
+  )
+}
+
+function Card({
+  className,
+  iconName,
+  title,
+  des,
+}: {
+  className?: string
+  iconName: string
+  title: string
+  des: string
+}) {
+  return (
+    <div
+      className={cn(
+        'border border-[#0000001F] border-solid rounded-3xl flex flex-col p-10 tracking-wide',
+        className
+      )}
+    >
+      <DynamicSvg iconName={iconName} className="w-[72px] h-[72px]" />
+      <h3 className="mt-10 font-semibold text-xl">{title}</h3>
+      <p className="mt-[14px] text-base leading-7 text-[#00000080]">{des}</p>
+    </div>
+  )
+}
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/header/index'
 import Section1 from '@/components/section-1/index'
 import Section2 from '@/components/section-2/index'
+import Section3 from '@/components/section-3'
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <Header className="px-6 py-4" />
       <Section1 className="mt-7" />
       <Section2 className="mt-[56px]" />
+      <Section3 className="px-6 mt-[120px]" />
     </>
   )
 }
