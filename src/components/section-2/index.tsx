c6f48fa013e7bbb66175b7f8ef33f5b345144af2
yangxiaohu <yangxiaohu2015@163.com>
2024-12-09 15:25:43 +0800

    feat: add section 2 layout and components

---
+++ src/components/section-2/index.tsx
@@ -0,0 +1,38 @@
+import { cn } from '@udecode/cn'
+
+import brand1 from '@/assets/brands/causal_brand.png'
+import brand2 from '@/assets/brands/openAi_brand.png'
+import brand3 from '@/assets/brands/causal_brand.png'
+import brand4 from '@/assets/brands/plain_brand.png'
+import brand5 from '@/assets/brands/passionfroot_brand.png'
+import brand6 from '@/assets/brands/dopt_brand.png'
+
+const BRANDS = [
+  { icon: brand1, url: '' },
+  { icon: brand2, url: '' },
+  { icon: brand3, url: '' },
+  { icon: brand4, url: '' },
+  { icon: brand5, url: '' },
+  { icon: brand6, url: '' },
+]
+
+export default function Section({ className }: { className?: string }) {
+  return (
+    <div
+      className={cn(
+        'flex items-center  flex-nowrap gap-[56px] px-6',
+        className
+      )}
+    >
+      {BRANDS.map((brand) => (
+        <a href={brand.url} key={brand.icon} className="flex-shrink-0">
+          <img
+            src={brand.icon}
+            className="h-[30px] w-auto object-contain"
+            alt="Brand logo"
+          />
+        </a>
+      ))}
+    </div>
+  )
+}
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,11 +1,13 @@
 import Header from '@/components/header/index'
 import Section1 from '@/components/section-1/index'
+import Section2 from '@/components/section-2/index'
 
 export default function HomePage() {
   return (
     <>
       <Header className="px-6 py-4" />
       <Section1 className="mt-7" />
+      <Section2 className="mt-[56px]" />
     </>
   )
 }
