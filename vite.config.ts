3001f010e3298a550b03e5faad0f77627897b0c5
markof <markof@163.com>
2024-12-11 18:52:50 +0800

    build: update env

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.CDN_ASSETS_PATH || undefined,
   plugins: [react()],
   resolve: {
     alias: {
