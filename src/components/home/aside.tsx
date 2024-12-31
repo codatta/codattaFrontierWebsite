8744e47184c3a8a8f1ed80f6e346ae281e318d20
yangxiaohu <yangxiaohu2015@163.com>
2024-12-31 17:25:28 +0800

    feat: add gradient border

--- src/components/home/aside.tsx
+++ src/components/home/aside.tsx
@@ -12,14 +12,19 @@ export default function Aside({ className }: TProps) {
   return (
     <div
       className={cn(
-        'relative flex w-[344px] flex-shrink-0 flex-col overflow-hidden rounded-2xl border border-transparent bg-gray-100',
+        'relative w-[344px] flex-shrink-0 overflow-hidden rounded-2xl border border-transparent',
         className
       )}
+      style={{
+        background: 'linear-gradient(180deg, rgba(255, 255, 255, 0.125) 0%, rgba(255, 255, 255, 0) 100%)'
+      }}
     >
-      <div className="absolute left-0 top-0 size-full bg-[#252532]"></div>
-      <UserInfoSection />
-      <CheckinSection />
-      <RewardSection />
+      <div className="flex size-full flex-col bg-gray-100">
+        <div className="absolute left-0 top-0 size-full bg-[#252532]"></div>
+        <UserInfoSection />
+        <CheckinSection />
+        <RewardSection />
+      </div>
     </div>
   )
 }
