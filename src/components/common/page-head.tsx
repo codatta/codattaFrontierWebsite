7847774d80794725078f4bde1cfa33d2de25cca1
ymwangel <921893011@qq.com>
2025-04-16 22:31:40 +0800

    feat: cmu

--- src/components/common/page-head.tsx
+++ src/components/common/page-head.tsx
@@ -1,7 +1,7 @@
 import LogoWhiteSvgImage from '@/assets/common/logo-white.svg'
 import { ArrowLeft } from 'lucide-react'
 
-export default function PageHead(props: { children: React.ReactNode }) {
+export default function PageHead(props: { children?: React.ReactNode }) {
   return (
     <div className="flex h-[84px] items-center justify-between border-b border-white/10 px-6 py-4">
       <div
