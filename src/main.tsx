0d8cd4fd5a60b03baaf7d1ded276d88a3590471a
Meow Lord <supermeowlord@gmail.com>
2025-06-19 17:19:33 +0800

    feat: update codatta-connect and change user network to bsc

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,7 @@ import duration from 'dayjs/plugin/duration'
 import timezone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
 import isToday from 'dayjs/plugin/isToday'
+import { defineChain } from 'viem'
 
 dayjs.extend(duration)
 dayjs.extend(utc)
@@ -28,12 +29,33 @@ if (!container) {
   throw new Error('root container not found')
 }
 
+const BSC_CHAIN = defineChain({
+  id: 56,
+  name: 'BNB Smart Chain Mainnet',
+  nativeCurrency: {
+    name: 'BNB',
+    symbol: 'BNB',
+    decimals: 18
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://bsc-dataseed1.bnbchain.org']
+    }
+  },
+  blockExplorers: {
+    default: {
+      name: 'BSCScan',
+      url: 'https://bscscan.com/'
+    }
+  }
+})
+
 initReactGA()
 
 const root = createRoot(container)
 root.render(
   <ConfigProvider theme={AntdTheme}>
-    <CodattaConnectContextProvider singleWalletName="Binance Wallet">
+    <CodattaConnectContextProvider singleWalletName="Binance Wallet" chains={[BSC_CHAIN]}>
       <Router></Router>
     </CodattaConnectContextProvider>
   </ConfigProvider>
