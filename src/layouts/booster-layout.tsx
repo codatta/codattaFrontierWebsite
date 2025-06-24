f117c11f7a21f2d46c19117488eb759daf360cf2
yangxiaohu <yangxiaohu2015@163.com>
2025-06-24 10:11:44 +0800

    feat: update booster page layout style

--- src/layouts/booster-layout.tsx
+++ src/layouts/booster-layout.tsx
@@ -4,7 +4,7 @@ import AuthChecker from '@/components/app/auth-checker'
 export default function BoosterLayout() {
   return (
     <AuthChecker>
-      <div className="relative min-h-screen bg-[#1c1c26] px-6 text-white">
+      <div className="relative min-h-screen bg-[#1c1c26] px-6 pb-10 text-white">
         <Outlet />
       </div>
     </AuthChecker>
