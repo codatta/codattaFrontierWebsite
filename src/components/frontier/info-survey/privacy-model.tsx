5d1e69fe5f945b491c3af9656b78fab5d40bbee4
yangxiaohu <yangxiaohu2015@163.com>
2025-08-26 14:34:24 +0800

    feat: update annotator modal style

--- src/components/frontier/info-survey/privacy-model.tsx
+++ src/components/frontier/info-survey/privacy-model.tsx
@@ -1,22 +1,31 @@
 import { Modal } from 'antd'
 import { Button } from '@/components/booster/button'
 
+import { useCountdown } from '@/hooks/use-countdown'
+import { useState } from 'react'
+
 export default function PrivacyModel({ open, onClose }: { open: boolean; onClose: () => void }) {
+  const [disabled, setDisabled] = useState(true)
+  const [seconds] = useCountdown(6, () => {
+    setDisabled(false)
+  })
+
   return (
-    <Modal open={open} onCancel={onClose} footer={null} centered closeIcon={false} width={600}>
-      <div className="p-6 text-center">
+    <Modal open={open} onCancel={onClose} footer={null} centered closeIcon={false} width={300}>
+      <div className="py-1 text-center">
         <h2 className="text-xl font-bold">Privacy Consent</h2>
-        <div className="mt-4 overflow-y-auto px-4 text-left indent-4">
-          <p>
-            By joining the Codatta Booster Campaign, you consent to the collection and use of your personal information.
-            Participation is voluntary, and your information will remain confidential.
-          </p>
-          <p>
-            Your response will be used exclusively for internal purposes to enhance our services and better understand
-            our users. We will not share your data with third parties without your consent.
-          </p>
+        <div className="mt-5 max-h-[300px] overflow-y-auto text-sm leading-[22px] text-[#BBBBBE]">
+          By joining the Codatta Booster Campaign, you consent to the collection and use of your personal information.
+          Participation is voluntary, and your information will remain confidential. Your response will be used
+          exclusively for internal purposes to enhance our services and better understand our users. We will not share
+          your data with third parties without your consent.
         </div>
-        <Button className="mt-6 min-w-[120px] !px-6" onClick={onClose} text="Continue" />
+        <Button
+          className="mt-8 w-full"
+          onClick={onClose}
+          text={disabled ? `Continue(${seconds}s)` : 'Continue'}
+          disabled={disabled}
+        />
       </div>
     </Modal>
   )
