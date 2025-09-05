51c0207c65e6eb48c01901b2d368b4a0c1cd05ff
Meow Lord <supermeowlord@gmail.com>
2025-09-05 14:13:54 +0800

    feat: week-11

--- src/router.tsx
+++ src/router.tsx
@@ -65,6 +65,7 @@ const FoodTplM2W1 = lazy(() => import('@/views/frontiers/food_tpl_m2_w1'))
 const FoodTplM2W234 = lazy(() => import('@/views/frontiers/food_tpl_m2_w234'))
 const FoodTplW9 = lazy(() => import('@/views/frontiers/food_tpl_w9'))
 const FoodTplW10 = lazy(() => import('@/views/frontiers/food_tpl_w9'))
+const FoodTplW11 = lazy(() => import('@/views/frontiers/food_tpl_w9'))
 const RoboticsTplW5 = lazy(() => import('@/views/frontiers/robitcs_tpl_w5'))
 
 const NFTGenerateTemplate = lazy(() => import('@/views/frontiers/form-type-7'))
@@ -227,6 +228,10 @@ export default function Router() {
           element={<FoodTplW10 templateId="FOOD_TPL_W10" />}
         />
         <Route
+          path="/frontier/project/FOOD_TPL_W11/:taskId/:questId"
+          element={<FoodTplW11 templateId="FOOD_TPL_W11" />}
+        />
+        <Route
           path="/frontier/project/ROBOTICS_TPL_W5/:taskId/:questId"
           element={<RoboticsTplW5 templateId="ROBOTICS_TPL_W5" />}
         />
@@ -248,6 +253,10 @@ export default function Router() {
         />
         <Route
           path="/frontier/project/ROBOTICS_TPL_W10/:taskId/:questId"
+          element={<RoboticsTplW5 templateId="ROBOTICS_TPL_W10" />}
+        />
+        <Route
+          path="/frontier/project/ROBOTICS_TPL_W11/:taskId/:questId"
           element={<RoboticsTplW5 templateId="ROBOTICS_TPL_W10" />}
         />
         <Route
@@ -327,6 +336,14 @@ export default function Router() {
           element={<CryptoTpl000004 templateId="CRYPTO_TPL_DEPOSIT_W10" />}
         />
         <Route
+          path="/frontier/project/CRYPTO_TPL_WITHDRAW_W11/:taskId/:questId"
+          element={<CryptoTpl000004 templateId="CRYPTO_TPL_WITHDRAW_W11" />}
+        />
+        <Route
+          path="/frontier/project/CRYPTO_TPL_DEPOSIT_W11/:taskId/:questId"
+          element={<CryptoTpl000004 templateId="CRYPTO_TPL_DEPOSIT_W11" />}
+        />
+        <Route
           path="/frontier/project/ANNOTATOR_INFO_SURVEY_BASIC/:taskId"
           element={<AnnotatorInfoSurveyBasic templateId="ANNOTATOR_INFO_SURVEY_BASIC" />}
         />
@@ -361,6 +378,11 @@ export default function Router() {
         <Route
           path="/frontier/project/KITCHEN_TPL_W10/:taskId/:questId"
           element={<KitchenAppData templateId="KITCHEN_TPL_W10" />}
+        />
+
+        <Route
+          path="/frontier/project/KITCHEN_TPL_W11/:taskId/:questId"
+          element={<KitchenAppData templateId="KITCHEN_TPL_W11" />}
         />
 
         <Route path="/app/submission/:submission_id/detail" element={<SubmissionDetail />}></Route>
