edac832254b1969bb933ee9c67b3d79a36cef52b
Meow Lord <supermeowlord@gmail.com>
2025-09-17 12:16:19 +0800

    feat: add dynamic menu

--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -1,16 +1,18 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Layout } from 'antd'
 import { cn } from '@udecode/cn'
 
-import SideNav from '@/components/app/app-sider'
+import SideNav, { MenuItem } from '@/components/app/app-sider'
 import AuthChecker from '@/components/app/auth-checker'
+import AirdropIcon from '@/assets/icons/app-nav/airdrop.svg'
 
 import { userStoreActions } from '@/stores/user.store'
 import { channelStoreActions, useChannelStore } from '@/stores/channel.store'
 
 export default function AppLayout({ className }: { className?: string }) {
   const { relatedInfo } = useChannelStore()
+  const [extraAppMenuItems, setExtraAppMenuItems] = useState<MenuItem[]>([])
 
   useEffect(() => {
     if (relatedInfo) channelStoreActions.updateRelatedInfo(relatedInfo)
@@ -18,13 +20,25 @@ export default function AppLayout({ className }: { className?: string }) {
 
   useEffect(() => {
     userStoreActions.getUserInfo()
+
+    setTimeout(() => {
+      setExtraAppMenuItems([
+        {
+          icon: <AirdropIcon color="white" />,
+          label: 'Airdrop',
+          isDynamic: true,
+          key: '/app/airdrop',
+          priority: 2.5
+        }
+      ])
+    }, 1000)
   }, [])
 
   return (
     <AuthChecker>
       <Layout className={cn('mx-auto h-screen max-w-[1168px] bg-transparent', className)}>
         <Layout.Sider className="bg-transparent" breakpoint="lg" width={248}>
-          <SideNav />
+          <SideNav dynamicMenuItems={extraAppMenuItems} />
         </Layout.Sider>
         <Layout.Content className="no-scrollbar min-w-0 flex-1 overflow-y-scroll py-7 pl-1 pr-4 lg:px-12 lg:py-8">
           <Outlet />
