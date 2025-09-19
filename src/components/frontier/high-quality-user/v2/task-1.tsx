36a62727bfceed77c498927fe3c3af7ab00099d6
yangxiaohu <yangxiaohu2015@163.com>
2025-09-19 15:40:30 +0800

    feat: high quality user update email bind logic

--- src/components/frontier/high-quality-user/v2/task-1.tsx
+++ src/components/frontier/high-quality-user/v2/task-1.tsx
@@ -1,22 +1,25 @@
-import { Button, message } from 'antd'
-import { Copy, FastForward, Info } from 'lucide-react'
-import CopyToClipboard from 'react-copy-to-clipboard'
-import { useEffect, useMemo } from 'react'
+import { Button, message, Modal } from 'antd'
+import { useEffect, useMemo, useState } from 'react'
 
 import { useUserStore, userStoreActions } from '@/stores/user.store'
+import { Mail } from 'lucide-react'
+import { shortenAddress } from '@/utils/wallet-address'
+import { CodattaConnect } from 'codatta-connect'
+import accountApi from '@/apis/account.api'
 
-// eslint-disable-next-line prettier/prettier
-export default function Task1({ onNext, isMobile }: { isMobile: boolean; onNext: (data: { email: string }) => Promise<boolean> }) {
+export default function Task1({
+  onNext,
+  isMobile
+}: {
+  isMobile: boolean
+  onNext: (data: { email: string }) => Promise<boolean>
+}) {
+  const [showWalletConnectModal, setShowWalletConnectModal] = useState(false)
   const { info } = useUserStore()
   const email = useMemo(() => info?.accounts_data?.find((account) => account.account_type === 'email')?.account, [info])
-  const link = `${window.location.protocol}//${window.location.host}/app/settings/personal`
-  const onCopied = () => {
-    message.success({
-      content: 'Copied to clipboard!'
-    })
-  }
+  const [newEmail, setNewEmail] = useState('')
   const onVerify = async () => {
-    let newEmail = email
+    let newEmail: string | undefined = email
 
     if (!newEmail) {
       const res = await userStoreActions.getUserInfo()
@@ -35,6 +38,21 @@ export default function Task1({ onNext, isMobile }: { isMobile: boolean; onNext:
       content: 'Please bind email first!'
     })
   }
+  async function handleEmailConnect(email: string, code: string) {
+    console.log(email, code)
+    await accountApi.bindEmail({
+      connector: 'codatta_email',
+      account_type: 'email',
+      account_enum: 'C',
+      email: email,
+      email_code: code
+    })
+    message.success('Email bind success.').then(() => {
+      setNewEmail(email)
+      userStoreActions.getUserInfo()
+      setShowWalletConnectModal(false)
+    })
+  }
 
   useEffect(() => {
     userStoreActions.getUserInfo()
@@ -42,7 +60,7 @@ export default function Task1({ onNext, isMobile }: { isMobile: boolean; onNext:
 
   return (
     <div>
-      <h2 className="mt-4 text-center text-2xl font-bold">Login & Bind Email</h2>
+      <h2 className="mt-4 text-center text-2xl font-bold">Bind Email</h2>
       <img
         src={isMobile ? '/codatta-banner.png' : '/codatta-banner-pc.png'}
         alt="high quality user task1 banner"
@@ -51,53 +69,47 @@ export default function Task1({ onNext, isMobile }: { isMobile: boolean; onNext:
       <p className="mt-4 rounded-xl bg-[#252532] px-4 py-[10px] text-center text-base text-white md:mt-6 md:border md:border-[#FFFFFF1F] md:bg-transparent md:text-left md:text-[#BBBBBE]">
         Bind your email to be the first to receive updates on upcoming high-reward tasks.
       </p>
-      <div className="mt-4 rounded-xl bg-[#252532] px-4 py-[10px] md:mt-6 md:border md:border-[#FFFFFF1F] md:bg-transparent md:text-left md:text-[#BBBBBE]">
-        <h2 className="flex items-center gap-1 text-base font-bold text-white">
-          <FastForward />
-          Steps to complete
-        </h2>
-        <ul className="mt-2 text-sm leading-[22px] text-[#BBBBBE]">
-          <li className="flex items-start gap-2">
-            <span className="mt-2 block size-[4px] shrink-0 rounded-full bg-[#BBBBBE]"></span>
-            <span className="flex-1">Copy the link below and open it in your browser</span>
-          </li>
-          <li className="mt-2">
-            <CopyToClipboard text={link} onCopy={onCopied}>
-              <div className="flex items-center justify-between gap-3 rounded-xl border border-[#FFFFFF1F] px-4 py-3">
-                <div className="flex flex-1 items-center gap-2 overflow-hidden text-base font-bold text-[#875DFF]">
-                  <span className="overflow-hidden text-ellipsis">{link}</span>
-                </div>
-                <Copy className="size-6 text-white" />
-              </div>
-            </CopyToClipboard>
-          </li>
-          <li className="mt-2 flex items-start gap-2">
-            <span className="mt-2 block size-[4px] shrink-0 rounded-full bg-[#BBBBBE]"></span>
-            <span className="flex-1">Login using your Binance wallet address from this event</span>
-          </li>
-          <li className="flex items-start gap-2">
-            <span className="mt-2 block size-[4px] shrink-0 rounded-full bg-[#BBBBBE]"></span>
-            <span className="flex-1">Bind your email (if not already bound)</span>
-          </li>
-          <li className="flex items-start gap-2">
-            <span className="mt-2 block size-[4px] shrink-0 rounded-full bg-[#BBBBBE]"></span>
-            <span className="flex-1">Return here and click verify to confirm</span>
-          </li>
-        </ul>
-      </div>
-      <div className="mt-4 rounded-xl bg-[#252532] px-4 py-[10px] md:mt-6 md:border md:border-[#FFFFFF1F] md:bg-transparent md:text-left md:text-[#BBBBBE]">
-        <h2 className="flex items-center gap-1 text-base font-bold text-white">
-          <Info className="size-5" />
-          Note
-        </h2>
-        <ul className="mt-2 list-inside list-disc text-sm leading-[22px] text-[#BBBBBE]">
-          <li>Already bound? Just login and return to verify. </li>
-          <li>Must use your event Binance wallet</li>
-        </ul>
-      </div>
-      <Button type="primary" className="mt-8 h-[44px] w-full rounded-full text-base font-bold" onClick={onVerify}>
-        Verify Now
-      </Button>
+      {!email && !newEmail ? (
+        <>
+          <Button
+            type="primary"
+            className="mt-8 h-[44px] w-full rounded-full text-base font-bold"
+            onClick={() => setShowWalletConnectModal(true)}
+          >
+            <Mail size={16} className="mr-2"></Mail> Connect Email
+          </Button>
+          <Modal
+            className="relative !max-w-[400px] [&_.ant-modal-content]:rounded-3xl [&_.ant-modal-content]:bg-[#252532]"
+            open={showWalletConnectModal}
+            onCancel={() => setShowWalletConnectModal(false)}
+            footer={null}
+            width={463}
+            centered
+            styles={{ content: { padding: 0 } }}
+            destroyOnClose
+          >
+            <CodattaConnect
+              onEmailConnect={handleEmailConnect}
+              config={{
+                showEmailSignIn: true,
+                showFeaturedWallets: false,
+                showMoreWallets: false,
+                showTonConnect: false
+              }}
+            ></CodattaConnect>
+          </Modal>
+        </>
+      ) : (
+        <>
+          <div className="flex items-center gap-2 rounded-lg border border-white/10 px-4 py-2">
+            <Mail size={16}></Mail>
+            <div className="truncate">{shortenAddress(email || newEmail, 16)}</div>
+          </div>
+          <Button type="primary" className="mt-8 h-[44px] w-full rounded-full text-base font-bold" onClick={onVerify}>
+            Confirm
+          </Button>
+        </>
+      )}
     </div>
   )
 }
