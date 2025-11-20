324cf5a39c8db9556e39a0cb291291840b3d2860
Meow Lord <supermeowlord@gmail.com>
2025-11-20 13:06:01 +0800

    feat: add qualification operatiion in physical questions form page

--- src/apis/frontiter.api.ts
+++ src/apis/frontiter.api.ts
@@ -1,5 +1,6 @@
 import { AxiosInstance } from 'axios'
 import request, { PaginationResponse, TPagination } from './request'
+import { TaskInfo } from './booster.api'
 
 interface Response<T> {
   data: T
@@ -51,6 +52,7 @@ export interface TaskDetail {
   question_status?: number // 1: available, 2: no more questions, 3. need to change question group
   data_requirements: unknown
   reward_info: readonly TaskRewardInfo[]
+  qualification_datas: TaskInfo[]
 
   status: 'PENDING' | 'SUBMITTED' | 'REFUSED' | 'ADOPT'
   txHashUrl: string
