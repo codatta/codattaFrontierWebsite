6b309234e3df420af4d82beb5c1033c5c1533b1e
Meow Lord <supermeowlord@gmail.com>
2025-11-20 14:34:33 +0800

    fix: fix build error

--- src/views/frontiers/physical_question.tsx
+++ src/views/frontiers/physical_question.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Form, Button, Spin } from 'antd'
 import { useParams } from 'react-router-dom'
 
