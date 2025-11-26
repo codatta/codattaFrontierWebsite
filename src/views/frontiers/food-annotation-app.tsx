b3a03a66a3ec2dbbbdc4cac6f00bb49e46188877
Meow Lord <supermeowlord@gmail.com>
2025-11-26 17:01:12 +0800

    fix: fix build error

--- src/views/frontiers/food-annotation-app.tsx
+++ src/views/frontiers/food-annotation-app.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo, useEffect, useCallback } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { message, Spin } from 'antd'
 import { ChevronsUpDown, X } from 'lucide-react'
 
