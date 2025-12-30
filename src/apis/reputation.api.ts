9613beaee074ed0b311b24c2a640e2715861b629
yangxiaohu <yangxiaohu2015@163.com>
2025-12-30 11:33:31 +0800

    feat: update top reputation api

--- src/apis/reputation.api.ts
+++ src/apis/reputation.api.ts
@@ -1,4 +1,5 @@
-import request, { PaginationParam, PaginationResponse } from './request'
+import { SummaryUserInfo } from '@/api-v1/user.api'
+import request, { PaginationParam, PaginationResponse, Response } from './request'
 
 class ReputationApi {
   async getReputations(pagination: PaginationParam) {
@@ -8,11 +9,24 @@ class ReputationApi {
     )
     return res.data
   }
+
+  async getTopReputations(pagination: PaginationParam = { page: 1, page_size: 50 }) {
+    const { data } = await request.post<Response<UserReputation[]>>('/v2/user/reputation/top', pagination)
+    return data
+  }
+
+  // TODO to repcale url
+  // async getTopContributions(pagination: PaginationParam = { page: 1, page_size: 50 }) {
+  //   const { data } = await request.post<Response<UserContribution[]>>('/user/contribution/top', pagination)
+  //   return data
+  // }
 }
 
 const reputationApi = new ReputationApi()
 export default reputationApi
 
+export type UserContribution = SummaryUserInfo & { contribute: string }
+export type UserReputation = SummaryUserInfo & { reputation: number | null }
 export enum ReputationType {
   Increase = 'INCREASE',
   Decrease = 'DECREASE'
